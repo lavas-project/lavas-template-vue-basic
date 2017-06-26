4c42e6321b40d90ae252f256ca5bd29a5b7fd42c
liuruoran <liuruoran@baidu.com>
2017-06-26 13:24:20 +0800

    basic增加polyfill

--- src/entry-client.js
+++ src/entry-client.js
@@ -3,7 +3,7 @@
  * @author *__ author __*{% if: *__ email __* %}(*__ email __*){% /if %}
  */
 
-import 'es6-promise/auto';
+import 'babel-polyfill';
 import FastClick from 'fastclick';
 import {createApp} from './app';
 
