da3b8d9535ecaf2ab1350f73b3b0933e821bec88
liuruoran <liuruoran@baidu.com>
2017-06-26 14:46:07 +0800

    basic demo 首页修改

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,11 @@
 <template>
     <div class="hello">
-        <h1>{{ msg }}</h1>
-        <h2>Essential Links</h2>
+        <h1 class="gray--text">LAVAS</h1>
+        <h4 class="gray--text">[ˈlɑ:vəz]</h4>
+
         <ul>
-            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-            <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-            <br>
-            <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-        </ul>
-        <h2>Ecosystem</h2>
-        <ul>
-            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-            <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+            <li><a href="https://github.baidu.com/lavas-project">Lavas Github</a></li>
+            <li><a href="https://lavas.baidu.com">Lavas 官网</a></li>
         </ul>
     </div>
 </template>
@@ -31,23 +21,37 @@
     }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
+h1 {
+    margin-top: 100px;
+    font-size: 46px;
+    font-weight: 500;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+h4 {
+    margin-bottom: 50px;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+ul {
+    padding: 0;
+}
 
-    a {
-        color: #42b983;
-    }
+li {
+    list-style: none;
+    display: inline-block;
+}
+
+a {
+    display: inline-block;
+    line-height: 45px;
+    width: 150px;
+    height: 45px;
+    font-weight: bold;
+    background: #2874f0;
+    text-decoration: none;
+    color: #fff;
+}
+
+
 </style>
+
