2a8389e84f25de0ba4872b36ca44d70cfc53b975
panyuqi <panyuqi@baidu.com>
2017-06-26 19:46:18 +0800

    fix: 同步修改

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,6 @@ var path = require('path');
 var utils = require('./utils');
 var config = require('../config');
 var vueLoaderConfig = require('./vue-loader.conf');
-require('babel-polyfill');
 
 function resolve(dir) {
     return path.join(__dirname, '..', dir);
@@ -17,8 +16,7 @@ function resolve(dir) {
 
 module.exports = {
     entry: {
-        app: ['babel-polyfill', './src/entry-client.js'],
-        skeleton: './src/entry-skeleton.js'
+        app: ['babel-polyfill', './src/entry-client.js']
     },
     output: {
         path: config.build.assetsRoot,
--- src/entry-client.js
+++ src/entry-client.js
@@ -7,8 +7,6 @@ import 'babel-polyfill';
 import FastClick from 'fastclick';
 import {createApp} from './app';
 
-import 'normalize.css';
-
 FastClick.attach(document.body);
 
 const {app, router} = createApp();
